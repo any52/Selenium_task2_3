55b81d5579b2bd3dbc7ebc19803a14cb8cd3ee36
Anna Moiseeva <belova_al@mail.ru>
2021-12-17 22:28:23 +0300

    LoginTest was fixed

--- src/test/java/ru.stqa.selenium/LoginTest.java
+++ src/test/java/ru.stqa.selenium/LoginTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class LoginTest {
     private WebDriver driver;
@@ -18,27 +16,12 @@ public class LoginTest {
     }
 
     @Test
-    public void registrationTest()  {
+    public void loginTest()  {
 
-        String password = "1234qwert";
-        driver.get("http://localhost/litecart/en/");
-        driver.findElement(By.linkText("New customers click here")).click();
-        driver.findElement(By.name("firstname")).sendKeys("Alina");
-        driver.findElement(By.name("lastname")).sendKeys("Ivanova");
-        driver.findElement(By.name("address1")).sendKeys("Sovetskay");
-        driver.findElement(By.name("address2")).sendKeys("Center");
-        driver.findElement(By.name("postcode")).sendKeys("101000");
-        driver.findElement(By.name("city")).sendKeys("Moscow");
-
-        WebElement selectElem = driver.findElement(By.name("country_code"));
-        Select selectCountry = new Select(selectElem);
-        selectCountry.selectByValue("RU");
-
-        driver.findElement(By.name("email")).sendKeys("alina_ivanova@mail.ru");
-        driver.findElement(By.name("phone")).sendKeys("89203459812");
-        driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.name("confirmed_password")).sendKeys(password);
-        driver.findElement(By.name("create_account")).click();
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @AfterEach
