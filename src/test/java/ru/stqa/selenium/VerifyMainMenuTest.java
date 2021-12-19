75eccd0185cc688a717ac55c1f6218652bda5892
Anna Moiseeva <belova_al@mail.ru>
2021-12-19 18:43:13 +0300

    Task 6 was addded

---
+++ src/test/java/ru/stqa/selenium/VerifyMainMenuTest.java
@@ -0,0 +1,68 @@
+package ru.stqa.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class VerifyMainMenuTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void start(){
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void mainMenuTest()  {
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        List<WebElement> pointsOfMenu = new ArrayList<>();
+        List<WebElement> subpointsOfMenu  = new ArrayList<>();
+        pointsOfMenu = driver.findElements(By.id ("app-"));
+        for (int i = 0; i < pointsOfMenu.size(); i ++){
+            pointsOfMenu.get(i).click();
+            Assertions.assertTrue(checkTitleOfPage());
+            pointsOfMenu.clear();
+            pointsOfMenu = driver.findElements(By.id ("app-"));
+            subpointsOfMenu = pointsOfMenu.get(i).findElements(By.className ("name"));
+            if (subpointsOfMenu.size()>0){
+                for (int j = 0; j < subpointsOfMenu.size(); j ++){
+                    subpointsOfMenu.get(j).click();
+                    Assertions.assertTrue(checkTitleOfPage());
+                    subpointsOfMenu.clear();
+                    pointsOfMenu.clear();
+                    pointsOfMenu = driver.findElements(By.id ("app-"));
+                    subpointsOfMenu = pointsOfMenu.get(i).findElements(By.className ("name"));
+                }
+            }
+        }
+
+    }
+    public boolean checkTitleOfPage(){
+        if((driver.findElements(By.cssSelector("#content h1")).size() == 0)){
+            return false;
+        };
+        return true;
+    }
+
+    @AfterEach
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
